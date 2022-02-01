8dacff641753ba56a98ad60fcbc900fa74e8e7fb
Ying Ying <yingyingfengg@gmail.com>
2022-01-31 23:58:42 -0500

    updated css

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan')
+const postBank = require('./postBank')
+
+app.use(morgan('dev'));
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    const posts = postBank.list()
+    res.send(`
+    <html>
+    <head>
+        <title>Wizarding World of Harry Potter</title>
+        <link rel ="stylesheet" href="/style.css" />
+    </head>
+        <body>
+            <div class="title">
+            <header>Harry Potter Books</header>
+            ${posts.map(post => `
+                <div class="info">
+                    <p>
+                        ${post.title}
+                    </p>
+                    <small class="date">
+                        Release Year: ${post.release}
+                    </small>
+                </div>`
+            ).join('')}
+            </div>
+        </body>
+    </html>
+    `)
+})
 
 const PORT = 3000;
 
